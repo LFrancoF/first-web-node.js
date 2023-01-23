6c2543b61457f6aeb42a543745cbc916d4f2b158
LFrancoF <g_itzlordposix_f@hotmail.com>
2023-01-23 01:01:22 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+import indexRoute from "./routes/index.js";
+//import ejs from 'ejs';   express tiene integracion con ejs por defecto, por tanto se puede quitar el import
+
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set("views", join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.use(indexRoute);
+
+app.use(express.static(join(__dirname, 'public')));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening at PORT: ${PORT}...`);
+});
